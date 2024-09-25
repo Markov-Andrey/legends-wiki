7d09943cdc74b6e10ba078293fb18a7c16e08f31
Andre Markov <andremarkov@icloud.com>
2024-09-25 13:18:54 +0300

    fix template

--- pages/legends.js
+++ pages/legends.js
@@ -2,12 +2,12 @@ function renderHeroGrid(heroes) {
     return `
         <div class="flex flex-wrap justify-center mx-auto w-full">
             ${heroes.map((hero) => `
-                <div class="hero-item flex-shrink-0 w-1/3 md:w-1/4 lg:w-1/5">
+                <div class="hero-item flex-shrink-0 w-1/4 md:w-1/5 lg:w-1/6 xl:w-1/7">
                     <div class="cursor-pointer filter hover:contrast-125 hover:brightness-125 transition duration-300" onclick="loadPage('${hero.page}')">
                         <img 
                             src="${hero.icon}" 
                             alt="${hero.name}" 
-                            class="object-cover w-full"
+                            class="object-cover w-full h-auto"
                         >
                     </div>
                 </div>
