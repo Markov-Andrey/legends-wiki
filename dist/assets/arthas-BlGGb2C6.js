83828d0a35df92229068591d940d02a23424bd0c
Andre Markov <andremarkov@icloud.com>
2025-03-11 17:13:41 +0300

    switch to vue js

---
+++ dist/assets/arthas-BlGGb2C6.js
@@ -0,0 +1,8 @@
+const e={name:"Arthas Lich King",subtitle:"Army of Scourge",image:"arthas.webp",mechanics:[{title:"Soul Harvest",description:"Destroying enemy units leaves souls that Arthas can harvest. It is an additional resource."},{title:"Runes",description:"There are three types of runes: Blood, Frost, and Unholy, each with 3 levels. You can allocate only 3 points in any combination."},{title:"Soul Reforging",description:"Allows you to upgrade party with souls, granting him a specialization in one of the runes."}],features:[{text:"Easy to learn",isPositive:!0},{text:"Adaptive army",isPositive:!0},{text:"No mages",isPositive:!1},{text:"Addiction to murder",isPositive:!1},{text:"Low team interaction",isPositive:!1}],history:`
+    <p>Born into nobility, Arthas Menethil was the prince of Lordaeron, destined for greatness.</p>
+    <p>From a young age, he trained as a paladin under the guidance of Uther the Lightbringer, embodying the virtues of honor and justice.</p>
+    <p>However, his relentless pursuit of power led him down a dark path, as he sought to eradicate the undead scourge threatening his homeland.</p>
+    <p>In a moment of desperation, Arthas made the fateful decision to wield the cursed runeblade Frostmourne, sacrificing his humanity for the sake of victory.</p>
+    <p>Transformed into the Lich King, Arthas became a harbinger of death and despair, commanding the Scourge in a relentless quest for domination.</p>
+    <p>His tragic fall from grace serves as a cautionary tale of ambition and the cost of power, forever echoing through the annals of history.</p>
+  `};export{e as characterData};
---
+++ dist/assets/uther-B63JAHKc.js
@@ -0,0 +1,8 @@
+const e={name:"Uther the Lightbringer",subtitle:"Paladins of the Silver Hand",image:"uther.webp",mechanics:[{title:"Divine Intervention",description:"Protects a unit with Divine Shield when health is low, but no more than 1 time per X min."},{title:"Summon Champions",description:"Summon 1 of 3 champions from one of the Paladin paths (Light, Protection, Retribution) to support the army with powerful auras."}],features:[{text:"Easy to learn",isPositive:!0},{text:"High armor",isPositive:!0},{text:"High healing potential",isPositive:!0},{text:"Low movement speed",isPositive:!1},{text:"Low army attack speed",isPositive:!1}],history:`
+            <p>Uther the Lightbringer, a noble paladin and the first of the Silver Hand, dedicated his life to the protection of the innocent and the eradication of evil.</p>
+            <p>Trained by the great prophet, Uther became a beacon of hope during the Third War, leading his fellow paladins against the growing threat of the Scourge.</p>
+            <p>His unwavering faith in the Light guided him through countless battles, earning him the respect of allies and foes alike.</p>
+            <p>Tragedy struck when he faced his former pupil, Arthas, now the Lich King, forcing Uther to confront the painful choices that led to Arthas' dark transformation.</p>
+            <p>Despite the sorrow of loss, Uther continued to fight for justice, becoming a symbol of resilience and sacrifice for future generations.</p>
+            <p>His legacy as a champion of the Light endures, inspiring those who seek to uphold honor and righteousness in the face of darkness.</p>
+        `};export{e as characterData};
