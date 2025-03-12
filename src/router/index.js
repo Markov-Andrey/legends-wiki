41e87282a229e3c6e9078cc79c1997725c257abd
Andre Markov <andremarkov@icloud.com>
2025-03-12 10:23:47 +0300

    upd to hash history

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 
 import Home from '@/pages/Home.vue'
 import Legends from "@/pages/Legends.vue";
@@ -45,7 +45,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes,
 })
 
