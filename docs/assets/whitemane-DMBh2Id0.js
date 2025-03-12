fe74391019795a6282196fdbc35dceb71c8e841f
Andre Markov <andremarkov@icloud.com>
2025-03-12 09:29:13 +0300

    fix images

---
+++ docs/assets/whitemane-DMBh2Id0.js
@@ -0,0 +1,6 @@
+const e={name:"Sally Whitemane",subtitle:"Scarlet Crusade",image:"/images/legends/whitemane.webp",mechanics:[{title:"Scarlet March",description:"A special mode in which units gradually lose health but gain increased movement speed and attack rate. The effect intensifies the longer the Scarlet Crusade remains active."}],features:[{text:"High Attack and Movement Speed",isPositive:!0},{text:"Accelerated Construction",isPositive:!0},{text:"Powerful Spellcasters",isPositive:!0},{text:"Requires High Micro-Management",isPositive:!1},{text:"Some Abilities and Effects Rely on Mechanics",isPositive:!1},{text:"Reduced Health for Units and Structures",isPositive:!1}],history:`
+            <p>A devoted priestess of the Light, Sally Whitemane's hatred for undeath was forged in tragedy when she was forced to slay her own family after they transformed into mindless undead.</p>
+            <p>She rose to become the ruthless High Inquisitor of the Scarlet Crusade, overseeing the Scarlet Monastery.</p>
+            <p>Though driven by a noble cause, her zeal turned to fanaticism, seeing all outsiders as tainted.</p>
+            <p>Gifted with the power to resurrect fallen crusaders, she ruled the Monastery with fear and unwavering conviction, making her one of the most formidable leaders of the Scarlet Crusade.</p>
+        `};export{e as characterData};
